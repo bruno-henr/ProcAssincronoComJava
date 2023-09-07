d4653fe5f590dc266e1aa04fa0a10e209e03406f
Bruno Henrique <brunohenriquex00@gmail.com>
2023-09-07 17:31:53 -0300

    Aprendendo sobre metodos utilizados em parallelStreams e suas diferencas

--- src/ParalelStreams.java
+++ src/ParalelStreams.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ParalelStreams {
     public static void main(String[] args) {
@@ -25,5 +27,41 @@ public class ParalelStreams {
         * Já com stream o resultado é diferente, por que se trata de apenas uma thread, entao o valor será sempre o primeiro da lista
         * */
         list.stream().findAny().ifPresent(System.out::println);
+        // skip
+        /*
+        * skip: pula um elemento da lista
+        * limit: limita o total de consultas
+        *
+        * com essa abordagem os elementos devem está sincronizados para seja possivel
+        * fazer o skip e limitar as consultas, pois assim se perde performace na aplicacao
+        * OBS: para resolver essa gargalo, é recomendado utilizar a funcao .unordered() !!
+        * Entao o skip vai poder pular quaisquer elemento da lista
+        * */
+        list.parallelStream()
+                .skip(1).limit(2)
+                .findAny().ifPresent(System.out::println);
+
+        // Collectors
+        /*
+        * toMap irá criar um map para cada elemento da lista e ao final juntar tudo em
+        * um só mapa
+        * Porem utilizando * toConcurrentMap * ira criar apenas um map e os elementos irao
+        * fazer suas insercoes dentro dessa Map, assim consumindo menos memoria
+        * */
+        Map<Integer, Boolean> collect = list.parallelStream()
+                .collect(
+                        Collectors.toMap(n -> n,n -> n % 2 == 0)
+                );
+        System.out.println(collect);
+        //groupinBy
+        /**
+         * Como o novo ja diz, ele vai agrupar por alguma coisa, nesse caso por numeros
+         * pares e impares
+         * */
+        Map<Boolean, List<Integer>> collect2 = list.parallelStream()
+                .collect(
+                        Collectors.groupingBy(n -> n % 2 == 0)
+                );
+        System.out.println(collect2);
     }
 }
